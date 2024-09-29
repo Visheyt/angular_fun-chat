ed822ed4a38f853aa1261c6b98978262db768e1e
Alex <fornikaras7@gmail.com>
2024-09-29 14:06:18 +0500

    feat: add not choosen chat message

--- src/app/chat/components/main-page/main-page.component.ts
+++ src/app/chat/components/main-page/main-page.component.ts
@@ -1,9 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { UsersListComponent } from '../users-list/users-list.component';
 import { MessagesComponent } from '../messages/messages.component';
-import { Subscription } from 'rxjs';
-import { Store } from '@ngrx/store';
-import { selectChat } from '../../../store/selectors/chat.selector';
 
 @Component({
   selector: 'app-main-page',
@@ -12,25 +9,4 @@ import { selectChat } from '../../../store/selectors/chat.selector';
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {
-  private store = inject(Store);
-
-  public isChatOpen: boolean = false;
-
-  private subscription = new Subscription();
-
-  public contactName: string = '';
-
-  constructor() {
-    this.subscription.add(
-      this.store.select(selectChat).subscribe((chat) => {
-        this.isChatOpen = chat.isOpen;
-        this.contactName = chat.contactName;
-      })
-    );
-  }
-
-  public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
-}
+export class MainPageComponent {}
--- src/app/chat/components/messages/messages.component.ts
+++ src/app/chat/components/messages/messages.component.ts
@@ -8,6 +8,10 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { Store } from '@ngrx/store';
+import { chatActions } from '../../../store/actions/chat.action';
+import { Subscription } from 'rxjs/internal/Subscription';
+import { selectChat } from '../../../store/selectors/chat.selector';
 
 @Component({
   selector: 'app-messages',
@@ -26,9 +30,32 @@ import { MatInputModule } from '@angular/material/input';
 export class MessagesComponent {
   private fb = inject(NonNullableFormBuilder);
 
-  public contactName = input.required<string>();
+  private store = inject(Store);
 
   public form = this.fb.group({
     message: ['', Validators.required],
   });
+
+  public chat = {
+    isOpen: false,
+    contactName: '',
+    isOnline: false,
+  };
+
+  private subscription = new Subscription();
+
+  constructor() {
+    this.subscription.add(
+      this.store.select(selectChat).subscribe((chat) => {
+        this.chat = chat;
+      })
+    );
+  }
+
+  public closeChat() {
+    this.store.dispatch(chatActions.close());
+  }
+  public ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
