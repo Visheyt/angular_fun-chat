a27e0f0bc6195733208f8e1d0f03ec7bee7db8a8
Alex <fornikaras7@gmail.com>
2024-10-17 17:17:50 +0500

    feat: add auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from './core/auth/login/login-page/login-page.component';
 import { MainPageComponent } from './chat/components/main-page/main-page.component';
+import { authGuard } from './core/auth/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: 'main',
     component: MainPageComponent,
+    canActivate: [authGuard],
   },
   {
     path: '',
